d16cd82b11422a39756bd4643d07c3840a5fb2dc
Bokume2 <nagatomoyuu2@gmail.com>
2025-12-22 13:34:08 +0900

    add: プロジェクトの雛形としてのtsxおよびcssファイルを追加

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [output, setOutput] = useState("")
+
+  return (
+    <>
+      <h1>Sui-BF</h1>
+    </>
+  )
+}
+
+export default App
